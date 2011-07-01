ce69445072e3239605164b20f77a49af274899d9
huchifeng <huchifeng@gmail.com>
2011-07-01 17:37:08 +0800

    try save photos to db

--- mygit-js.cmd
+++ mygit-js.cmd
@@ -1,5 +1,5 @@
 /* 2>nul
-del 1.db
+rem del 1.db
 ..\myjs\myjs %~f0 ..\mygit\1.db || goto :err
 ls -l 1.db
 mygit dir --db 1.db
@@ -10,6 +10,7 @@ exit /b
 
 */
 try{
+	//test();
 	main();
 }
 catch(e){
@@ -18,6 +19,48 @@ catch(e){
 }
 
 function main(){
+	// process redirect output "dir /s /b /a:-d" not reliable
+	app.eval(app.loadTextFile("json2.js"));
+
+	var db_file = app.argv()[2];
+	cmd_prefix = "..\\mygit\\mygit add --db "+db_file+" --type file --file ";
+
+	var m = [];
+	save_recursive(m, "e:/"); // 2011-04-24/
+
+	var lib = app.loadLibrary("../mygit/mygitlib.dll");
+	git = lib.create("createMyGit");
+	if(!git.open(db_file))
+		throw("open fail:"+db_file);
+	//app.log(JSON.stringify(m));
+	if(!git.add_data("list", JSON.stringify(m)))
+		throw("save list fail");
+}
+function save_recursive(m, p){
+	var all = app.dir(p);
+	for(var i=0; i<all.length; i++){
+		var p1 = p+all[i];
+		if(app.fileInfo(p1).isDir){
+			save_recursive(m, p1+"/");
+			continue;
+		}
+		//var id = git.add_file("file", p1);
+		//if(!id)
+		//	throw("add file fail"+p1);
+		var cmd = cmd_prefix + p1;
+		app.log(cmd);
+		var proc = app.createProcess(cmd);
+		proc.waitForReadyRead(-1); // otherwise no data
+		var id = proc.readLine(1000, false);
+		proc.close();
+		app.log(id);
+		id = Number(id);
+		app.log(p1+":"+id);
+		m.push({path:p1, id:id});
+	}
+}
+
+function test(){
 	var lib = app.loadLibrary("../mygit/mygitlib.dll");
 	git = lib.create("createMyGit");
 	//app.log(git);
