db2155863f32fcf8670c194d48d25083857de183
huchifeng <huchifeng@gmail.com>
2011-08-13 21:49:22 +0800

    ps example

--- ps--pdf/test-ps-to-pdf.cmd
+++ ps--pdf/test-ps-to-pdf.cmd
@@ -1,7 +1,8 @@
 path %ProgramFiles%\gs\gs8.71\bin;%path%
 path %ProgramFiles%\gs\gs8.71\lib;%path%
-start ps2pdf.bat gscjk_ag.ps tmp.pdf
-gswin32c --help
+rem start ps2pdf.bat gscjk_ag.ps tmp.pdf
+rem gswin32c --help
 
-gswin32c -sDEVICE=pdfwrite -sOutputFile=tmp2.pdf -dBATCH -q -dNOPAUSE gscjk_ag.ps
+gswin32c -sDEVICE=pdfwrite -sOutputFile=tmp1.pdf -dBATCH -q -dNOPAUSE gscjk_ag.ps
+gswin32c -sDEVICE=pdfwrite -sOutputFile=tmp2.pdf -dBATCH -q -dNOPAUSE image-qa.ps
 pause
