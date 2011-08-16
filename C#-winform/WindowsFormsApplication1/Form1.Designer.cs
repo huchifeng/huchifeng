6ebb12b52b4b4a31426eec3e9087ee62d006e5c0
huchifeng <huchifeng@gmail.com>
2011-08-16 16:02:15 +0800

    about resource and app.config

--- C#-winform/WindowsFormsApplication1/Form1.Designer.cs
+++ C#-winform/WindowsFormsApplication1/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -49,12 +51,34 @@
             this.textBox1.Size = new System.Drawing.Size(388, 21);
             this.textBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "访问resx";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "访问app.config";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 262);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,6 +91,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- C#-winform/WindowsFormsApplication1/Form1.cs
+++ C#-winform/WindowsFormsApplication1/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace WindowsFormsApplication1
 {
@@ -21,6 +22,25 @@ namespace WindowsFormsApplication1
         {
             i++;
             textBox1.Text = "Hello, World:" + i+","+System.DateTime.Now.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Resource1.String1); // 访问资源
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 微软不太建议动态写入app.config
+            // 添加引用 System.configuration
+            string s = "AppSettings:";
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                string value = ConfigurationManager.AppSettings[key];
+                s += "\n"+key + ":" + value + ";";
+            }
+            MessageBox.Show(s+"\n"+ConfigurationManager.AppSettings["aaa"]);
+            //MessageBox.Show(System.Configuration.ConfigurationManager.GetSection("userSettings")["aaa"]);
         }
     }
 }
