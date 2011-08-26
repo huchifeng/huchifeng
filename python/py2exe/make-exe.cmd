5f04d232e1be54e1e19b7a8045f46fddba9286d2
huchifeng <huchifeng@gmail.com>
2011-08-26 13:08:54 +0800

    py2exe, single exe file

--- python/py2exe/make-exe.cmd
+++ python/py2exe/make-exe.cmd
@@ -1,13 +1,27 @@
 set path=c:\python26\;%path%
 cd /d %~dp0
 
+python setup.py py2exe --help
+
 python setup.py py2exe
+move dist dist1
+rem 一个 dist 目录下得到 exe, library.zip, *.pyd, pytho26.dll 等
+
+python setup.py py2exe --bundle-files 1
+rem 得到单个 exe 文件
+
 pause
 exit /B
 
-dist 目录下得到 exe
+
 
 python 2.6
   msvcr90.exe 9.0.21022.8
   Microsoft Visual C++ 2008 Redistributable Package published 29-11-2007, 
   so not the VS2008 SP1 one (tested with Python 2.7.1).
+
+值为1表示pyd和dll文件会被打包到exe文件中，且不能从文件系统中加载python模块；
+值为2表示pyd和dll文件会被打包到exe文件中，但是可以从文件系统中加载python模块。
+另外setup中使用zipfile=None可以不生成library.zip。 
+
+
--- python/py2exe/setup.py
+++ python/py2exe/setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 import py2exe
-setup(console=['test1.py'])
+setup(console=['test1.py'], zipfile=None)
