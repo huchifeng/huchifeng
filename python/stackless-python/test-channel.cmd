57d8187c885bb6686a49e856a2e51ee590867d52
huchifeng <huchifeng@gmail.com>
2011-09-06 14:22:17 +0800

    stackless-python

---
+++ python/stackless-python/test-channel.cmd
@@ -0,0 +1,29 @@
+﻿"""
+  cd /d %~dp0
+  set path=c:\python32;%path%
+  python %0
+  pause
+  exit /b
+
+
+"""
+
+import stackless
+
+c = stackless.channel()
+def sender(chan, value):
+    chan.send(value)
+    print("sender done")
+t=stackless.tasklet(sender)(c, 5)
+t.run() # 创造出第二个 tasklet，否则因无tasklet可接收，会block唯一tasklet
+print("received:", c.receive())
+
+
+def receiver(chan):
+    print("i am waiting...")
+    value = chan.receive()
+    print("i got:", value)
+stackless.tasklet(receiver)(c)
+stackless.run()
+print("now sending...")
+c.send(500)
---
+++ python/stackless-python/test-tasklet.cmd
@@ -0,0 +1,25 @@
+﻿"""
+  cd /d %~dp0
+  set path=c:\python32;%path%
+  python %0
+  pause
+  exit /b
+
+Tasklets — Lightweight threads
+
+"""
+
+import stackless
+
+def func(*args, **kwargs):
+    print("scheduled with", args, "and", kwargs)
+    print(id(stackless.current))
+t=stackless.tasklet(func)(1, 2, 3, string="test")
+print(stackless.main == stackless.current); # true
+print(stackless.current.is_main) # true
+print(stackless.runcount) # 2
+t2=stackless.tasklet(func)(12, 22, 32, string="test")
+t.run() 
+# t 执行后 t2 也会执行
+# 如果 t2.run 则 t 不会执行，因为它在 chain 的前面
+
