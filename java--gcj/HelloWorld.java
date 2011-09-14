ee6e7113b750b661a6c3fdf458995d1c10fbd011
huchifeng <huchifeng@gmail.com>
2011-09-14 13:48:41 +0800

    java--gcj

---
+++ java--gcj/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+public static void main(String[] args)
+{
+  System.out.println("Hello, world: courtesy of GCJ!");
+  System.out.println("user.name = "+System.getProperty("user.name"));
+  System.out.println("user.home = "+System.getProperty("user.home"));
+}
+}
